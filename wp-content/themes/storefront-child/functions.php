0f9204668003abb2ef7e02c5ac3c61c01509448c
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-24 23:03:39 +0200

    editing sort order blocks in product listings

---
+++ wp-content/themes/storefront-child/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function jb_add_text_to_sort_order()
+{
+    echo '<span>' . 'Sort: ' . '</span>';
+}
+
+function jb_change_sorting_block_products_list()
+{
+    remove_action('woocommerce_after_shop_loop', 'woocommerce_catalog_ordering', 10);
+    add_action('woocommerce_before_shop_loop', 'jb_add_text_to_sort_order', 9);
+}
+
+add_action('after_setup_theme', 'jb_change_sorting_block_products_list');
