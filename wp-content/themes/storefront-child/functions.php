e80bac4e8e7f02333290ebe67e91cb43f426282a
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-25 10:09:00 +0200

    remove sidebar and make fullwidth all woocommerce pages

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -11,4 +11,12 @@ function jb_change_sorting_block_products_list()
     add_action('woocommerce_before_shop_loop', 'jb_add_text_to_sort_order', 9);
 }
 
+function jb_remove_storefront_sidebar()
+{
+    if (is_woocommerce()) {
+        remove_action('storefront_sidebar', 'storefront_get_sidebar', 10);
+    }
+}
+
 add_action('after_setup_theme', 'jb_change_sorting_block_products_list');
+add_action('get_header', 'jb_remove_storefront_sidebar');
